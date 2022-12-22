6c73ba8049bc16ac3d04d0413d5a250d23c39935
tomibandoni <tomasbandoni@hotmail.com>
2022-12-21 21:59:33 -0300

    Trabajo Practico en grupo

---
+++ ClaseTP/App.js
@@ -0,0 +1,76 @@
+// punto 1
+/*
+let nombre = prompt("Ingrese su nombre")
+console.log( "Su nombre es", nombre )
+
+// punto 2
+let edad = parseInt(prompt("Ingrese su edad"))
+console.log( "Su edad es", edad )
+
+// punto 3 y punto 6
+let hijos = prompt("¿Tiene hijos? SI/NO");
+
+if(hijos == "si"){
+    tieneHijos = true;
+    console.log("El usuario tiene hijos", tieneHijos)
+}else 
+    if(hijos == "no"){
+    tieneHijos = false;
+    console.log("El usuario no tiene hijos", tieneHijos)
+}else
+    console.log("La palabra ingresada es incorrecta")
+
+// punto 4
+let salario = parseInt(prompt("Ingrese su salario actual"))
+let salarioAnual = salario*12;
+console.log("Su salario anual es de", salarioAnual)
+
+
+//punto 5
+let edad = parseInt(prompt("Ingrese su edad"))
+if(edad >= 18){
+    console.log("Eres mayor de edad")
+} else console.log("Eres menor de edad")
+
+// punto 7
+let genero = prompt("Ingrese genero: hombre / mujer");
+
+if(genero == "hombre"){
+    console.log("El usuario es hombre")
+}else if(genero == "mujer"){
+    console.log("El usuario es mujer")
+}else
+    console.log("La palabra ingresada es incorrecta")
+
+// punto 8 y 9
+
+let lado = parseInt(prompt("Ingrese el valor de un lado"))
+let perimetro = lado*4;
+console.log("El perimetro es", perimetro)
+
+// punto 10
+
+let numero1 = parseInt(prompt("Ingrese el primer valor"))
+let numero2 = parseInt(prompt("Ingrese el segundo valor"))
+let numero3 = parseInt(prompt("Ingrese el tercer valor"))
+let numero4 = parseInt(prompt("Ingrese el cuarto valor"))
+
+let suma = numero1+numero2;
+console.log("suma igual", suma)
+let multiplicacion = numero3*numero4;
+console.log("Multiplicacion igual", multiplicacion)
+
+// punto 11
+let numero = parseInt(prompt("ingrese un numero"))
+
+if(numero > 0 && numero <=9){
+    console.log("el numero tiene un solo digito")
+}
+else if (numero >= 10 && numero <=99)
+{
+    console.log("el numero es de dos digitos")
+}
+else{
+    console.log("el numero ingresado es incorrecto")
+}
+*/
